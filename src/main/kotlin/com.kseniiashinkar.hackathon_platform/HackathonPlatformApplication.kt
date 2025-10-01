c4faac0bfff74093575587a286e5b8e107d3d8e8
Kseniia Shinkar <k3361603@gmail.com>
2025-10-01 12:51:27 +0300

    Add files via upload

---
+++ src/main/kotlin/com.kseniiashinkar.hackathon_platform/HackathonPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.kseniiashinkar.hackathon_platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackathonPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<HackathonPlatformApplication>(*args)
+}
